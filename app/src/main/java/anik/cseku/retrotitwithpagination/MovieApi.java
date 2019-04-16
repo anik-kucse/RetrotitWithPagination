be2a590bf8bd67f9db0d03edb419dafa6559d4c1
Ashfiqur Rahman <anik.ku.cse@gmail.com>
2019-04-16 17:50:27 +0600

    Only loads first 20 data. Then goes into looping

---
+++ app/src/main/java/anik/cseku/retrotitwithpagination/MovieApi.java
@@ -0,0 +1,20 @@
+package anik.cseku.retrotitwithpagination;
+
+import android.content.Context;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MovieApi {
+    public static Retrofit retrofit = null;
+
+    public static Retrofit getClient(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("https://api.themoviedb.org/3/")
+                    .build();
+        }
+        return  retrofit;
+    }
+}
